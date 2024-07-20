d469be0d31368fb1c6a3cde7ac51809dc3b1e8d9
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-20 23:15:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+function compareNumbers() {
+  let num1 = prompt("Enter the first number:");
+  let num2 = prompt("Enter the second number:");
+  num1 = Number(num1);
+  num2 = Number(num2);
+  let result = (num1 === num2);
+  alert(result);
+}
+compareNumbers();
